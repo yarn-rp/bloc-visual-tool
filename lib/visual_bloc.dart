a399ca1e67abb76e0ac467e688b3f77de258b473
Yan Rodriguez <yanrodriguez@Yans-MBP.attlocal.net>
2024-05-02 18:02:15 -0400

    first commit

---
+++ lib/visual_bloc.dart
@@ -0,0 +1,10 @@
+/// visual_bloc, Bloc Visual Tooling
+///
+/// ```sh
+/// # activate visual_bloc
+/// dart pub global activate visual_bloc
+///
+/// # see usage
+/// visual_bloc --help
+/// ```
+library visual_bloc;
